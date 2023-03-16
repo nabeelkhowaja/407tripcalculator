0acb53be98df36ac53103a2f5c49d5a82338ae75
nabeelkhowaja <nabeel.molwani@gmail.com>
2023-03-16 12:37:21 -0400

    Added images to be used in README.md file.

--- cost-calculator-service/src/main/java/com/trip/costcalculatorservice/CostCalculatorServiceApplication.java
+++ cost-calculator-service/src/main/java/com/trip/costcalculatorservice/CostCalculatorServiceApplication.java
@@ -15,6 +15,7 @@ public class CostCalculatorServiceApplication {
     }
 
     public static void main(String[] args) {
+
         SpringApplication.run(CostCalculatorServiceApplication.class, args);
     }
 
